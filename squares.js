48c586799da6c9bcd109771b453ce2fc9ed11fa0
edwardlee14 <7edwardlee@gmail.com>
2023-06-28 16:46:33 -0700

    almost done

--- squares.js
+++ squares.js
@@ -1,28 +1,48 @@
 function gridMaker() {
-    let list = document.getElementById("grid");
-    for (let i = 0; i < 4; i++) {
+    var slider = document.getElementById('slider');
+    var sliderValue;
+  
+    noUiSlider.create(slider, {
+      start: [50],
+      range: {
+        'min': 0,
+        'max': 100
+      },
+      step: 1
+    });
+  
+    slider.noUiSlider.on('update', function (values, handle) {
+        sliderValue = Math.round(values[handle]);
+      let list = document.getElementById("grid");
+      list.innerHTML = "";
+      for (let i = 0; i < sliderValue; i++) {
         let row = document.createElement("div");
         row.classList.add("gridRows");
-        for (let j = 0; j < 4; j++) {
-            let box = document.createElement("div");
-            box.classList.add("box");
-            row.appendChild(box);
+        for (let j = 0; j < sliderValue; j++) {
+          let box = document.createElement("div");
+          box.classList.add("box");
+          row.appendChild(box);
         }
         list.appendChild(row);
-    }
-}
-
-function marker() {
-    const boxes = document.querySelectorAll(".box");
-    boxes.forEach(function(cube) {
-        cube.addEventListener("mouseover", function() {
-            cube.style.backgroundColor = "blue";
-        });
-        cube.addEventListener("mouseout", function() {
-            cube.style.backgroundColor = "blue";
-        });
+      }
+      marker();
+      let size = document.getElementById("dimensions");
+      size.classList.add("dimensions");
+      size.textContent = sliderValue + " by " + sliderValue;
     });
-}
-
-marker();
-gridMaker();
+    marker();
+  }
+  
+  function marker() {
+    const boxes = document.querySelectorAll(".box");
+    boxes.forEach(function (cube) {
+      cube.addEventListener("mouseover", function () {
+        cube.style.backgroundColor = "blue";
+      });
+      cube.addEventListener("mouseout", function () {
+        cube.style.backgroundColor = "blue";
+      });
+    });
+  }
+  
+  gridMaker();
